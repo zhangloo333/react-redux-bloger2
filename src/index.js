b2e1b7a8dee83e12aada5d1471d5f28feeca4eac
abc <ebookcode123@gmail.com>
2017-04-05 22:49:26 -0700

    add router file in index.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
-import App from './components/app';
+// delte app
+// import App from './components/app';
 import reducers from './reducers';
+
+// add new components
+import routes from './routes';
+
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    {/* <App />*/}
   </Provider>
   , document.querySelector('.container'));
