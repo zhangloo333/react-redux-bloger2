c619c89732eff5d82ca21ee1def863f17782cbe7
ebookcode123 <ebookcode123@gmail.com>
2017-04-06 12:39:54 -0700

    80 fetching data with lift cycle componentWillMount, 当组件第一次被执行的时候，invoke

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,9 +1,24 @@
 import React, {Component} from 'react';
 
-
-
 //76
 
-export default () => {
-  return <div>List of Blog posts.</div>
-}
+// export default () => {
+//   return <div>List of Blog posts.</div>
+// }
+
+
+//80 fetching data
+class PostIndex extends Component {
+  componentWillMount() {
+    console.log('this would be a good time to call acntion')
+    // this.props.fetchPosts();
+
+  }
+  render() {
+    return (
+      <div> List of blog posts</div>
+    );
+  }
+ }
+
+export default PostIndex
