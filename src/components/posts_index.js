feb481996bda114946c10dcaaecef52857c04b85
ebookcode123 <ebookcode123@gmail.com>
2017-04-06 12:46:51 -0700

    81 合并 component to container 然后输出。

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,17 +1,22 @@
 import React, {Component} from 'react';
 
-//76
+//81
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import {fetchPosts} from '../actions/index';
 
+
+//76
 // export default () => {
 //   return <div>List of Blog posts.</div>
 // }
 
-
 //80 fetching data
 class PostIndex extends Component {
   componentWillMount() {
-    console.log('this would be a good time to call acntion')
-    // this.props.fetchPosts();
+    // console.log('this would be a good time to call acntion')
+    //81
+    this.props.fetchPosts();
 
   }
   render() {
@@ -21,4 +26,14 @@ class PostIndex extends Component {
   }
  }
 
-export default PostIndex
+// export default PostIndex
+
+// bind container with components
+ function mapDispatchToProps(dispatch) {
+   return bindActionCreators({fetchPosts}, dispatch);
+ }
+
+// export default connect(null,mapDispatchToProps)(PostIndex);
+//上面的简便写法
+// export default connect(null,{fetchPosts:fetchPosts})(PostIndex);
+export default connect(null,{fetchPosts})(PostIndex);
