9c0b98256d33617ca3c354b8850dd22c1710c189
ebookcode123 <ebookcode123@gmail.com>
2017-04-06 12:29:32 -0700

    79  怎么fetch data 送入 reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux';
 
+import PostsReducer from './reducer_posts';
+// add in 78 back to redux
+
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  // state: (state = {}) => state 删除了不用了
+  posts: PostsReducer
 });
 
 export default rootReducer;
