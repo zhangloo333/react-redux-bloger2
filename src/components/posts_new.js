826ed6cd738496c060523c8d1ce2685b87553497
ebookcode123 <ebookcode123@gmail.com>
2017-04-06 17:11:37 -0700

    85 在posts_new里面建利一个form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,12 +1,49 @@
 import React, {Component} from 'react';
-
+import {reduxForm} from 'redux-form';
 
 class PostsNew extends Component {
   render() {
     return (
-      <div> Create Form </div>
+      // <div> Create Form </div>  build a form
+      <form>
+        <h3>Create A New Post</h3>
+
+        <div className = "form-group">
+          <label>Title</label>
+          <input type="text" className="form-control" />
+        </div>
+
+        <div className = "form-group">
+          <label>categories</label>
+          <input type="text" className="form-control" />
+        </div>
+
+        <div className = "form-group">
+          <label>Content</label>
+          <input type="text" className="form-control" />
+        </div>
+
+        <button type ="submit" className="btn btn-primary">Submit</button>
+      </form>
     )
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+    form: 'PostsNewForm',
+    fields:['title','categories','content']
+})(PostsNew);
+
+// user types something in ...record it on application state
+// redux form 主要是把 componenet level 变成 application level的
+/*
+state ==={
+  form: {
+    PostsNewForm: {
+      title: '.....',
+      categories: '.....',
+      content: '.......'
+    }
+  }
+}
+*/
