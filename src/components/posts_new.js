62a91ca26cbed5506617c824d926e79f47686b8f
abc <ebookcode123@gmail.com>
2017-04-06 23:42:36 -0700

    add validate function

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -19,7 +19,10 @@ class PostsNew extends Component {
 
         <div className = "form-group">
           <label>Title</label>
-          <input type="text" className="form-control" {...title}/>
+          <input type="text" className="form-control" placeholder = { title.touched ? title.error : ""} {...title}/>
+          <div className="text-help">
+            { title.touched ? title.error : ""}
+          </div>
         </div>
 
         <div className = "form-group">
@@ -38,6 +41,18 @@ class PostsNew extends Component {
   }
 }
 
+// add valiad form
+function validate(values) {
+  const errors = {};
+  if(!values.title) {
+    errors.title = 'Enter a username';
+  }
+  return errors;
+}
+
+
+
+
 /* 87
 connect: frist argument is mapStateToProps, 2nd is mapDispathToProps
 reduxForm: 1ft is form congig, 2nd is mapStateToProps, 3rd is mapDispathToProps
@@ -47,7 +62,8 @@ reduxForm: 1ft is form congig, 2nd is mapStateToProps, 3rd is mapDispathToProps
 
 export default reduxForm({
     form: 'PostsNewForm',
-    fields:['title','categories','content']
+    fields:['title','categories','content'],
+    validate
 },null,{createPost})(PostsNew);
 
 // user types something in ...record it on application state
