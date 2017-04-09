a70af2ba6dd4342b66751eb15ff0d298ceda3069
abc <ebookcode123@gmail.com>
2017-04-08 18:29:35 -0700

    96 添加一个delete的action的功能

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,6 +8,9 @@ export const CREATE_POST = 'CREATE_POST';
 //93 FETHCH 单个的data
 export const FETCH_POST = 'FETCH_POST';
 
+//95 detlet action
+export const DELETE_POST = 'DELETE_POST';
+
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY ='?key=leoz123455';
 
@@ -35,6 +38,13 @@ export function fetchPost(id) {
   return {
     type:FETCH_POST,
     payload: request
+  }
+}
 
+export function deletePost(id){
+  const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`);
+  return {
+    type: DELETE_POST,
+    payload: request
   }
 }
--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -2,12 +2,19 @@ import React, {Component} from 'react';
 
 //93 把reducer 里面装入state里面的东西调入 component
 import { connect } from 'react-redux';
-import {fetchPost} from '../actions/index';
+import {fetchPost,deletePost} from '../actions/index';
+
+//94 add linker
+import {Link} from 'react-router';
 
 class PostsShow extends Component {
   componentWillMount(){
     this.props.fetchPost(this.props.params.id);
   }
+  onDeleteClick(){
+    this.props.deletePost(this.props.params.id);
+  }
+
   render() {
     const {post} = this.props;
     console.log(this.props.post);
@@ -16,6 +23,11 @@ class PostsShow extends Component {
     }
     return (
       <div>
+        <Link to="/">Back To Index</Link>
+        <button className="btn btn-danger pull-xs-right"
+        onClick={this.onDeleteClick.bind(this)}>
+          Delete Post
+        </button>
         <h3>{post.title}</h3>
         <h6>Categories: {post.categories}</h6>
         <p>{post.content}</p>
@@ -28,4 +40,4 @@ function mapStateToProps(state) {
   return {post: state.posts.post};
 }
 
-export default connect(mapStateToProps,{fetchPost})(PostsShow);
+export default connect(mapStateToProps,{fetchPost,deletePost})(PostsShow);
